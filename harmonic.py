ffdf0fd4a046f7f536d63b49a1cd12e7581d8e84
uday pspk <udaykumar90599@gmail.com>
2020-11-27 22:40:34 +0530

    Refactor:Added comments and Header file

--- harmonic.py
+++ harmonic.py
@@ -1,16 +1,25 @@
+"""
+* author - ${USER}
+* date - ${11/23/2020}
+* time - ${3:30 pm}
+* package - ${PACKAGE_NAME}
+* Title - Prints the Nth harmonic number: 1/1 + 1/2 + ... + 1/N.
+"""
 class Harmonic :
-    def __init__(self):
+	#creating constructor
+ def __init__(self):
         self.number
     def HarmonicValue(n):
         harmonicValue = 0
         harmonicNumber = int(input("Enter harmonic number: "))
-        if n > 0:
+	#Taking input values 
+       if n > 0:
             for num in range(1, n + 1):
                 harmonicValue = 1 / num
             else:
                 print("number should be greater than zero")
         return harmonicValue
-
+#main method
 if __name__ == "__main__":
     try :
         object=Harmonic()
