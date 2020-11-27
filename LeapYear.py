1e85f39cc81f1c254ce37b2f78d3f59f9e862d06
uday pspk <udaykumar90599@gmail.com>
2020-11-27 20:28:04 +0530

    Refactor:Added header file and comments

--- LeapYear.py
+++ LeapYear.py
@@ -3,25 +3,27 @@
 * date - ${11/23/2020}
 * time - ${3:30 pm}
 * package - ${PACKAGE_NAME}
-* Title - Replace of Username by using User Input
+* Title - check given year is leap or not
 """
 
 class Leapyear :
-    def __init__(self):
-        self.number
-    def checkLeapYear(year):
-        if len(str(year)) == 4:
-            if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-                print(str(year) + " is Leap year")
+    def __init__(self,yearValue):
+        self.number=yearValue
+    def checkLeapYear(self):
+#checking a year is leap year or not
+        if len(str(self.number)) == 4:
+            if (self.number % 4 == 0 and self.number % 100 != 0) or self.number % 400 == 0:
+                print(str(self.number) + " is Leap year")
+            else:
+                print(str(self.number) + " is not Leap year")
         else:
-            print(str(year) + " is not Leap year")
-    else:
             print( "Please enter valid year")
-yearValue = int(input("Enter year: "))
+
+#Main Method
 if __name__ == "__main__":
     yearValue = int(input("Enter year: "))
     try :
-        object=Leapyear()
+        object=Leapyear(yearValue)
         object.checkLeapYear()
     except :
         print('Exception Occured')
